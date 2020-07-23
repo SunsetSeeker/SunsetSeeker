5ce9407325c8eb958c49166a37619ed86816d06b
vmayrock <verena.mayrock@gmail.com>
2020-07-23 15:31:20 +0200

    cloudinary stuff

--- sunsetseeker/routes/sunset.js
+++ sunsetseeker/routes/sunset.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const uploader = require('../cloudinary'); 
 const Sunset = require('../models/Sunset');
 
 router.post('/list', (req, res) => {
