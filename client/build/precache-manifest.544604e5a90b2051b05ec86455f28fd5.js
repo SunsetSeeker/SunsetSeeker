9b3ea17f407b4b02c7d4a282e206d93d8a4b1849
Gabriel Palladini <gmpalladini@gmail.com>
2020-07-30 16:56:42 +0200

    suncatcher

---
+++ client/build/precache-manifest.544604e5a90b2051b05ec86455f28fd5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa75487eaa823bbddc550ff86a2e9e7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "010037b6629c01e6b3de",
+    "url": "/static/css/2.149b3d71.chunk.css"
+  },
+  {
+    "revision": "37d9b29d117ba440a0ce",
+    "url": "/static/css/main.ed409de5.chunk.css"
+  },
+  {
+    "revision": "010037b6629c01e6b3de",
+    "url": "/static/js/2.43607907.chunk.js"
+  },
+  {
+    "revision": "5599e968862052a1e150ba50f6354fc6",
+    "url": "/static/js/2.43607907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d9b29d117ba440a0ce",
+    "url": "/static/js/main.1e0fe9a5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "6d4113e245b8ad9313e83a3921040d3d",
+    "url": "/static/media/background-app.6d4113e2.jpg"
+  },
+  {
+    "revision": "ec5a50b6aede37c121135ea70aadbd0f",
+    "url": "/static/media/carousel1.ec5a50b6.jpg"
+  },
+  {
+    "revision": "3397dcaffb5a18a4a5849217d31f0064",
+    "url": "/static/media/carousel2.3397dcaf.jpg"
+  },
+  {
+    "revision": "b1208f7efcb88d0459113a6ff90514d5",
+    "url": "/static/media/carousel3.b1208f7e.jpg"
+  },
+  {
+    "revision": "7a0bf9ecc84cb074deac5e2ab7002cd9",
+    "url": "/static/media/carousel4.7a0bf9ec.jpg"
+  },
+  {
+    "revision": "01c38200b8801fdef19b76ca7c5e498a",
+    "url": "/static/media/carousel5.01c38200.jpg"
+  },
+  {
+    "revision": "2f5dc04642d6c77e75bf54ea680e0c8d",
+    "url": "/static/media/carousel6.2f5dc046.jpg"
+  },
+  {
+    "revision": "f1a81f16ee0dea01ddee60202e16fe2b",
+    "url": "/static/media/carousel7.f1a81f16.jpg"
+  },
+  {
+    "revision": "0b1e5c66ff3a8182adf594c54f08f630",
+    "url": "/static/media/google-logo.0b1e5c66.png"
+  },
+  {
+    "revision": "107c2a142813dd2c7d3d77bc35f41a99",
+    "url": "/static/media/trash.107c2a14.png"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3b0fe82d51bd15028f9e3fee36900a45.js"
+  "/precache-manifest.544604e5a90b2051b05ec86455f28fd5.js"
 );
 
 self.addEventListener('message', (event) => {
