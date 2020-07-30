010e47ca9bd504bd38f81996f9145bb9ea4fd86d
vmayrock <verena.mayrock@gmail.com>
2020-07-30 19:23:09 +0200

    final

--- client/src/components/AddSpot/AddSpot.js
+++ client/src/components/AddSpot/AddSpot.js
@@ -1,37 +1,36 @@
 import * as React from "react";
-// import './AddSpot.css';
+import "./AddSpot.css";
 import { Component } from "react";
-import axios from 'axios'; 
-import { FaPen } from 'react-icons/fa';
-// import FileInput from './FileInput'; 
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import { FaPen } from "react-icons/fa";
+// import FileInput from './FileInput';
+import { Link } from "react-router-dom";
 import Pin from "./Pin";
 import { render } from "react-dom";
 import MapGL, { Marker } from "react-map-gl";
 import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import ReactMapGL from "react-map-gl";
 
-
 export default class AddSpot extends Component {
-  state={
-    title:"", 
-    description:"",
-    file:[], 
-    uploadText: "Choose a photo", 
+  state = {
+    title: "",
+    description: "",
+    file: [],
+    uploadText: "Choose a photo",
     viewport: {
       latitude: 52,
       longitude: 13,
       zoom: 10,
       width: 600,
       height: 400,
-      coordinates:"",
+      coordinates: "",
     },
     marker: {
       latitude: 52.5196,
       longitude: 13.4069,
     },
     events: {},
-  }; 
+  };
   componentDidMount = () => {
     navigator.geolocation.getCurrentPosition((response) => {
       this.setState({
@@ -48,75 +47,75 @@ export default class AddSpot extends Component {
         },
       });
     });
-  };   
-onDrop=(picture)=> {
+  };
+  onDrop = (picture) => {
     this.setState({
-      file:this.state.file.concat(picture)
-    }); 
-  }   
-  handleChange = event => {
-    const name=event.target.name; 
-    const value=event.target.value; 
-    this.setState({
-      [name]: value
+      file: this.state.file.concat(picture),
     });
-  }; 
-  handleFile = element => {    
+  };
+  handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
     this.setState({
-      // uploadOn:true, 
-      uploadText:"It's uploading.."
-    }); 
+      [name]: value,
+    });
+  };
+  handleFile = (element) => {
+    this.setState({
+      // uploadOn:true,
+      uploadText: "It's uploading..",
+    });
     console.log(element);
-    const uploadData=new FormData();
-    console.log("SHOW ME THIS"+element.target) 
-    uploadData.append("img", element.target.files[0]); 
+    const uploadData = new FormData();
+    console.log("SHOW ME THIS" + element.target);
+    uploadData.append("img", element.target.files[0]);
     // for (var x=0; x<element.target.file.length; x++) {
     //   uploadData.append("img", element.target.file[x])
     // }
-    console.log("THIS IS HAPPENING")
+    console.log("THIS IS HAPPENING");
     axios
-    .post("/server/list/upload", uploadData)
-    .then(response =>{
-      this.setState({
-        file:[...this.state.file,response.data.secure_url], 
-        // uploadOn: false, 
-        uploadText: "Upload successful."
+      .post("/server/list/upload", uploadData)
+      .then((response) => {
+        this.setState({
+          file: [...this.state.file, response.data.secure_url],
+          // uploadOn: false,
+          uploadText: "Upload successful.",
+        });
       })
-    })
-    .catch(err=> console.log(err))
+      .catch((err) => console.log(err));
   };
-  handleSubmit = event => {
-    event.preventDefault(); 
+  handleSubmit = (event) => {
+    event.preventDefault();
     axios
-    .post('/server/list', {
-      title: this.state.title, 
-      description: this.state.description,
-      file: this.state.file,
-      latitude: this.state.marker.latitude,
-      longitude: this.state.marker.longitude,
+      .post("/server/list", {
+        title: this.state.title,
+        description: this.state.description,
+        file: this.state.file,
+        latitude: this.state.marker.latitude,
+        longitude: this.state.marker.longitude,
       })
-    .then((res) => {
-      console.log(res.data);
-      // this.props.getData();
-      this.props.history.push(`/spotdetails/${res.data._id}`);   
-      //added this again in order to check file upload to Mongo
-      this.setState({
-        title:" ", 
-        description:" ", 
-        file: " ", 
-      })        
-//     })
-//     .then((res) => {
-//       console.log(res)
-//       this.setState({
-//         title:" ", 
-//         description:" ", 
-//         file:" ", 
-//       }); 
-    })
-    .catch(err => {
-      console.log(err); 
-    });
+      .then((res) => {
+        console.log(res.data);
+        // this.props.getData();
+        this.props.history.push(`/spotdetails/${res.data._id}`);
+        //added this again in order to check file upload to Mongo
+        this.setState({
+          title: " ",
+          description: " ",
+          file: " ",
+        });
+        //     })
+        //     .then((res) => {
+        //       console.log(res)
+        //       this.setState({
+        //         title:" ",
+        //         description:" ",
+        //         file:" ",
+        //       });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   _updateViewport = (viewport) => {
     this.setState({ viewport });
@@ -145,88 +144,57 @@ onDrop=(picture)=> {
     });
   };
   removeFile(f) {
-    this.setState({file:this.state.file.filter((x) => x!==f)}); 
+    this.setState({ file: this.state.file.filter((x) => x !== f) });
   }
-  render(){
+  render() {
     const { viewport, marker } = this.state;
     // console.log(this.state.file)
     // console.log(viewport, marker)
-    return(
-      <div>
-        <button><Link to ={`/list`}>Go back toverview</Link></button>
-      <h2>Add a new sunset location:</h2>
-      <form 
-      onSubmit={this.handleSubmit}
-      encType="multipart/form-data"
-      >
-        <label>Name of the place:</label> 
-            <input 
-            type="text" 
-            id="title"
-            name="title"
-            value={this.state.title}
-            onChange={this.handleChange}
-            required
-            />
-        <label>Tell us more about the place:</label>
+    return (
+      <div className="addspot-container">
+        <Link to={`/list`}>
+          {" "}
+          <button className="white-button">Go back to overview</button>
+        </Link>
+        <h4>Add a new sunset location:</h4>
+        <form onSubmit={this.handleSubmit} encType="multipart/form-data">
+          <div className="input-container">
+            <label>Name of the place:</label> <br />
             <input
-            type="text"
-            id="description"
-            name="description"
-            value={this.state.description}
-            onChange={this.handleChange}
+              className="add-input"
+              type="text"
+              id="title"
+              name="title"
+              value={this.state.title}
+              onChange={this.handleChange}
+              required
             />
-            <div className="fileInput">
-              <label htmlFor="image" className="editButton">
-                <FaPen />
-              </label>
-              <input 
-              type="file" 
-              id="photo"
-              name="photo"
-              data-cloudinary-field="img_id"
-              //style={{display: "none"}}
-              // onChange={this.handleFile}
-              onChange={(event)=> this.handleFile(event)}
-              />
-              {this.state.uploadText}
-              {this.state.uploadText=="It's uploading.." && (
-              <div className="bouncing-loader">
-              <div></div>
-              <div></div>
-              <div></div>
-              </div>              
-              )}
-              <br/>
-              <br/>
-              {/* {this.state.file.map((f) => (
-                <div className="filepreview" onClick={this.removeFile.bind(this, f)}>{f.name}</div>
-              ))} */}
-              {/* showing the pictures when uploaded: */}
-              {this.state.file.length !==0 && this.state.file.map(pic=>{
-                return <>
-                <img src={pic} style={{width: "100px"}}></img>
-                </>
-              })}
           </div>
-          {/* <FileInput handleFile={this.handleFile} /> */}
-              {this.state.uploadText!=="It's uploading.." && (
-               <button type="submit" value="Add"> Add this spot</button> 
-              )}
-      </form>
-      <br/><br/><br/>
-      <div>
-      <ReactMapGL
-        {...this.state.viewport}
-        mapStyle="mapbox://styles/paolagaray/ckd0bdux30v981ilig8zxzd8p"
-        onViewportChange={(viewport) => this.setState({viewport})}
-        mapboxApiAccessToken={ process.env.REACT_APP_MAPBOX_TOKEN }
-      >
-        <span className="DragAndDrop MediumTextBold">
-            Drag and drop the pin to spot a hidden place
-        </span>
-            <Marker
-
+          <div className="input-container">
+            <label>Tell us more about the place:</label>
+            <br />
+            <input
+              className="add-input"
+              type="text"
+              id="description"
+              name="description"
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="map">
+            <label>Choose a spot on the map:</label>
+            <br />
+            <ReactMapGL
+              {...this.state.viewport}
+              mapStyle="mapbox://styles/paolagaray/ckd0bdux30v981ilig8zxzd8p"
+              onViewportChange={(viewport) => this.setState({ viewport })}
+              mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+            >
+              <span className="DragAndDrop MediumTextBold">
+                {/* Drag and drop the pin to spot a hidden place */}
+              </span>
+              <Marker
                 longitude={marker.longitude}
                 latitude={marker.latitude}
                 offsetTop={-20}
@@ -237,10 +205,57 @@ onDrop=(picture)=> {
                 onDragEnd={this._onMarkerDragEnd}
               >
                 <Pin size={20} />
-            </Marker>
-      </ReactMapGL>
+              </Marker>
+            </ReactMapGL>
+          </div>
+          <div className="input-container">
+            {this.state.uploadText}
+            {this.state.uploadText == "It's uploading.." && (
+              <div className="bouncing-loader">
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+            )}
+
+            <div className="fileInput">
+              <label htmlFor="image" className="editButton">
+                <FaPen />
+              </label>
+              <input
+                type="file"
+                id="photo"
+                name="photo"
+                data-cloudinary-field="img_id"
+                //style={{display: "none"}}
+                // onChange={this.handleFile}
+                onChange={(event) => this.handleFile(event)}
+              />
+              <br />
+            </div>
+
+            {/* {this.state.file.map((f) => (
+                <div className="filepreview" onClick={this.removeFile.bind(this, f)}>{f.name}</div>
+              ))} */}
+            {/* showing the pictures when uploaded: */}
+            {this.state.file.length !== 0 &&
+              this.state.file.map((pic) => {
+                return (
+                  <>
+                    <img src={pic} style={{ width: "100px" }}></img>
+                  </>
+                );
+              })}
+          </div>
+          {/* <FileInput handleFile={this.handleFile} /> */}
+          {this.state.uploadText !== "It's uploading.." && (
+            <button className="button" type="submit" value="Add">
+              {" "}
+              Add this spot
+            </button>
+          )}
+        </form>
       </div>
-      </div>
-    )
+    );
   }
 }
